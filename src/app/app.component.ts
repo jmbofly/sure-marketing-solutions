2d5fed565e6efdb9e0d3de89bf788670bb75a16b
jimi flynn <flynn-3199@cotc.edu>
2020-03-13 13:42:55 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sure-marketing-solutions';
+}
