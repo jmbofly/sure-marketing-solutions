8e8e50e64db8d2c0c84fa2ca0c64b86fca5770e3
jimi flynn <flynn-3199@cotc.edu>
2020-03-20 04:03:49 -0400

    added widget: quote-section

---
+++ src/app/shared/quote-section/quote-section.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from 'src/app/core/data.service';
+export interface Quote {
+  first?: string;
+  last?: string;
+  guidance?: string;
+  phone?: string;
+  message?: string;
+}
+@Component({
+  selector: 'sms-quote-section',
+  templateUrl: './quote-section.component.html',
+  styleUrls: ['./quote-section.component.scss']
+})
+export class QuoteSectionComponent implements OnInit {
+  @Input() text = `Ready to take the next step, or just need some direction? Fill out this form for a free consultation and quote.`
+
+  quoteModel: Quote = {
+    first: '',
+    last: '',
+    guidance: '',
+    phone: '',
+    message: '',
+  }
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+  }
+
+  async addNewQuoteSubmition(data: Quote) {
+    await this.dataService.addToCollection<Quote>('quotes', data).then(res => console.log('quote submitted', res))
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CarouselModule, PopoverModule, CollapseModule, AlertModule, BsDropdownModule, ModalModule, TabsModule, TooltipModule, PaginationModule, TypeaheadModule, ProgressbarModule, SortableModule, ButtonsModule, AccordionModule, TimepickerModule } from 'ngx-bootstrap';
+import { QuoteSectionComponent } from './quote-section/quote-section.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [QuoteSectionComponent],
   imports: [
     CommonModule,
+    FormsModule
   ],
   exports: [CommonModule, FormsModule,
     CarouselModule,
@@ -23,6 +25,8 @@ import { CarouselModule, PopoverModule, CollapseModule, AlertModule, BsDropdownM
     SortableModule,
     ButtonsModule,
     AccordionModule,
-    TimepickerModule,]
+    TimepickerModule,
+    QuoteSectionComponent,],
+  entryComponents: [QuoteSectionComponent]
 })
 export class SharedModule { }
